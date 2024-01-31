57e41de4b02034f0f71a4c8f52ffada720e1eed2
Nuhazet Correa Torres <nuhazet6@gmail.com>
2024-01-31 16:03:58 +0000

    not changed

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,10 @@
 ssh nuhazet@nuhazet.arkania.es "
   cd travelroad_django
   git pull
-
   source .venv/bin/activate
   pip install -r requirements.txt
-
+  supervisorctl restart travelroad
   # python manage.py migrate
   # python manage.py collectstatic --no-input
 
-  supervisorctl restart travelroad
+"  
